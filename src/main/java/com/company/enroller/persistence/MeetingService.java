fd70e02f2a5b4eb03ddabb4e2e2b467c87884478
harych <harych@gmail.com>
2021-05-03 16:29:34 +0200

    Homework, GOLD implementation

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -3,24 +3,76 @@ package com.company.enroller.persistence;
 import java.util.Collection;
 
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 
 @Component("meetingService")
 public class MeetingService {
 
 	DatabaseConnector connector;
+	Session session;
 
 	public MeetingService() {
 		connector = DatabaseConnector.getInstance();
+		session = connector.getSession();
 	}
 
 	public Collection<Meeting> getAll() {
-		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
-		return query.list();
+		return session.createCriteria(Meeting.class).list();
+	}
+	
+	public Meeting findById(Long id) {
+		Meeting meeting = (Meeting)session.get(Meeting.class, id);
+		return meeting;
+	}
+	
+	public void add(Meeting meeting) {
+		Transaction transaction = session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+	}
+	
+	public void delete(Meeting meeting) {
+		Transaction transaction = session.beginTransaction();
+		session.delete(meeting);
+		transaction.commit();	
+	}
+	
+	public void addMeetingParticipant(Meeting meeting, Participant participant) {
+		Transaction transaction = session.beginTransaction();
+		meeting.addParticipant(participant);
+		session.save(meeting);
+		transaction.commit();
+	}
+	
+	public void removeMeetingParticipant(Meeting meeting, Participant participant) {
+		Transaction transaction = session.beginTransaction();
+		meeting.removeParticipant(participant);
+		session.save(meeting);
+		transaction.commit();
+	}
+	
+	public void updateMeetingTitle(Meeting meeting, String title) {
+		Transaction transaction = session.beginTransaction();
+		meeting.setTitle(title);
+		session.save(meeting);
+		transaction.commit();
+	}
+	public void updateMeetingDescription(Meeting meeting, String description) {
+		Transaction transaction = session.beginTransaction();
+		meeting.setDescription(description);
+		session.save(meeting);
+		transaction.commit();
+	}
+	public void updateMeetingDate(Meeting meeting, String date) {
+		Transaction transaction = session.beginTransaction();
+		meeting.setDate(date);
+		session.save(meeting);
+		transaction.commit();
 	}
 
 }
